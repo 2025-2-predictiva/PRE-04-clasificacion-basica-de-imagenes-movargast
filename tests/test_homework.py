6e4874c2504508adfb3d758a8039cc53283518d6
movargast <movargast@unal.edu.co>
2025-09-20 11:14:22 -0500

    Entrega_Taller4

--- tests/test_homework.py
+++ tests/test_homework.py
@@ -16,7 +16,7 @@ def test_01():
     if not os.path.exists("homework/estimator.pickle"):
         raise FileNotFoundError("homework/estimator.pickle not found")
 
-    with open("homework/estimator.pkl", "rb") as file:
+    with open("homework/estimator.pickle", "rb") as file:
         new_clf = pickle.load(file)
 
     accuracy = accuracy_score(
